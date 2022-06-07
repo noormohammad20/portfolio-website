7a45929df5f6e5b62593b2bfa9657a292406ceb1
noormohammad20 <mdsaim204@gmail.com>
2022-06-07 14:47:41 +0600

    navbar created and inital design complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar/Navbar"
+import './App.css'
 function App() {
   return (
     <div className="App">
-      I am hare with new setting
+      <Navbar />
     </div>
   )
 }
