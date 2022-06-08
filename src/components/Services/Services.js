a8baae93d931699d210d6f596c666b920540b85f
noormohammad20 <mdsaim204@gmail.com>
2022-06-08 16:31:46 +0600

    complete the services section

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -3,6 +3,8 @@ import './Services.css'
 import HeartEmoji from "../../img/heartemoji.png"
 import Glasses from "../../img/glasses.png"
 import Humble from "../../img/humble.png"
+import Card from '../Card/Card'
+import Resume from './resume of noor_mohammad.pdf'
 const Services = () => {
     return (
         <div className='services'>
@@ -13,14 +15,40 @@ const Services = () => {
                 <span>I'm basically a front-end developer
                     <br />
                     but i can also do server side work with node expressJs and mongoDB </span>
-                <button className='button s-button'>Download Resume</button>
+                <a href={Resume} download>
+                    <button className='button s-button'>Download Resume</button>
+                </a>
+
                 <div className="blur s-blur" style={{ background: '#ABF1FF94' }}></div>
             </div>
             {/* right div */}
             <div className="cards">
-                I am Right Side
+                {/* first card  */}
+                <div style={{ left: '14rem' }}>
+                    <Card
+                        emoji={HeartEmoji}
+                        heading={'Design'}
+                        details={'figma , adobe ,photoshop'}
+                    />
+                </div>
+                {/* second card  */}
+                <div style={{ top: '12rem', left: '-4rem' }}>
+                    <Card
+                        emoji={Glasses}
+                        heading={'Developer'}
+                        details={'figma , adobe ,photoshop'}
+                    />
+                </div>
+                {/* third card  */}
+                <div style={{ top: '19rem', left: '12rem' }}>
+                    <Card
+                        emoji={Humble}
+                        heading={'UI/UX'}
+                        details={'figma , adobe ,photoshop'}
+                    />
+                </div>
             </div>
-
+            <div className='blur s-blur2' style={{ background: 'var--(purple)' }}></div>
         </div>
     )
 }
