92e31fd2164b5b158536134eac9f658e6e1c5cce
noormohammad20 <mdsaim204@gmail.com>
2022-06-08 12:53:19 +0600

    create the banner section and style it

---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import './Intro.css'
+import Vector1 from "../../img/Vector1.png"
+import Vector2 from "../../img/Vector2.png"
+import boy from "../../img/boy.png"
+import glassesimoji from "../../img/glassesimoji.png"
+import thumbup from "../../img/thumbup.png"
+import crown from "../../img/crown.png"
+
+import Github from '../../img/github.png'
+import Linkedin from '../../img/linkedin.png'
+import Instagram from '../../img/instagram.png'
+import FloatingDiv from '../FloatingDiv/FloatingDiv'
+const Intro = () => {
+    return (
+        <div className="intro">
+            <div className="i-left">
+                <div className="i-name">
+                    <span>Hey! I Am</span>
+                    <span>Noor Mohammad</span>
+                    <span>JavaScript developer with knowing some framework for the frontend and the backend</span>
+                </div>
+                <button className="button i-button">Hire me</button>
+
+                <div className="i-icons">
+                    <img src={Github} alt="" />
+                    <img src={Linkedin} alt="" />
+                    <img src={Instagram} alt="" />
+                </div>
+            </div>
+
+            <div className="i-right">
+                <img src={Vector1} alt="" />
+                <img src={Vector2} alt="" />
+                <img src={boy} alt="" />
+                <img src={glassesimoji} alt="" />
+                <div style={{ top: '-4%', left: '68%' }}>
+                    <FloatingDiv
+                        image={crown}
+                        text1="JavaScript"
+                        text2="Developer"
+                    />
+                </div>
+
+                <div style={{ top: '23rem', left: '0.5rem' }}>
+                    <FloatingDiv
+                        image={thumbup}
+                        text1="MERN STack"
+                        text2="Developer"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Intro
