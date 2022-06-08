a5e18ac8f4aecf3fa0574c32385c26ba743e71ca
noormohammad20 <mdsaim204@gmail.com>
2022-06-08 22:50:16 +0600

    install swiper js for sliding projects and create protfolio section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,8 +1,40 @@
 import React from 'react'
-
+import { Swiper, SwiperSlide } from 'swiper/react'
+import './Portfolio.css'
+import Manufacturer from '../../img/manufacturer.png'
+import Warehouse from '../../img/warehouse.png'
+import Learn from '../../img/learn.png'
+import Watch from '../../img/watch.png'
+import "swiper/css"
 const Portfolio = () => {
     return (
-        <div>
+        <div className='portfolio'>
+            {/* heading */}
+            <span>Recent Projects</span>
+            <span>Portfolio</span>
+
+            {/* slider */}
+
+            <Swiper
+                spaceBetween={30}
+                slidesPerView={3}
+                grabCursor={true}
+                className="portfolio-slider"
+            >
+                <SwiperSlide>
+                    <img src={Manufacturer} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Warehouse} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Learn} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Watch} alt="" />
+                </SwiperSlide>
+
+            </Swiper>
 
         </div>
     )
