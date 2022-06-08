1ac365d69af5d88d37156cae74c016ad6117d534
noormohammad20 <mdsaim204@gmail.com>
2022-06-08 16:42:54 +0600

    completed the experience section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from "./components/Navbar/Navbar"
 import './App.css'
 import Intro from "./components/Intro/Intro"
 import Services from "./components/Services/Services"
+import Experience from "./components/Experience/Experience"
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
       <Services />
+      <Experience />
     </div>
   )
 }
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -2,7 +2,23 @@ import React from 'react'
 import './Experience.css'
 const Experience = () => {
     return (
-        <div>
+        <div className='experience'>
+            <div className="achievement">
+                <div className="circle">1+</div>
+                <span>Years</span>
+                <span>Experience</span>
+            </div>
+            <div className="achievement">
+                <div className="circle">5+</div>
+                <span>Completed</span>
+                <span>Projects</span>
+            </div>
+            <div className="achievement">
+                <div className="circle">2+</div>
+                <span>Companies</span>
+                <span>Work</span>
+
+            </div>
 
         </div>
     )
